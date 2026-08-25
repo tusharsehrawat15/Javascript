f64c7eb093bc337e5f4435dd1e5b7de360c22877
tusharsehrawat15 <tusharsehrawat15@gmail.com>
2026-08-26 03:14:31 +0530

    Update 03_conversionOperations.js

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -22,3 +22,30 @@ console.log(booleanIsLoggedIn);
 ""=> false
 "tushar" => true
 */
+
+// ****************Operations********************
+
+let value = 3
+ let negValue = -value
+//  console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " tushar" 
+let str3 = str1 + str2;
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1+ "2");
+// console.log("1"+ 2 + 2);   // Don'g write all of such confusing codes. Improve readability
+// console.log(1+ 2 + "2");
+
+// console.log((3+4) * 5 % 3);
+
+console.log(true);
