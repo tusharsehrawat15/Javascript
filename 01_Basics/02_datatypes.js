fd689c767676a452aaff5ed308afd654f737d712
tusharsehrawat15 <tusharsehrawat15@gmail.com>
2026-08-26 00:30:20 +0530

    Add files via upload

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version.
+
+// alert( 3 + 3) // we are using nodejs, not browser, it works in browser.
+
+console.log(3
+    +
+    3) // code should be properly readable
+
+console.log("Tushar")
+
+
+let name= "Tushar" 
+let age = 20 
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean=> true / false
+//null => standalone value
+// undefined=> value has not been defined, zero and undefined, both are different
+// symbol => unique
+
+//object
+
+console.log(typeof "Tushar"); //tells the data type of the entered element // string
+console.log(typeof null) // object
+console.log(typeof undefined)// undefined
