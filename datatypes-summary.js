8841be459ae11f97714b3db731ae7f7a3fec83a9
tusharsehrawat15 <tusharsehrawat15@gmail.com>
2026-08-27 21:58:48 +0530

    Create datatypes-summary.js

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types: String, Number, Boolean , null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+consnole.log(id=== anotherId);
+
+// const bigNumber = 234343482959278237427n
+
+
+
+// Reference (Non Primitive)
+
+//Array, Objects, Functions
+
+const heros = ["Hulk", "Batman", "Superman"];
+let myObj = {
+    name: "Tushar",
+    age : 20,
+}
+
+const myFunction = function(){
+    console.log("hello World");
+}
+
+console.log(typeof anotherId);
