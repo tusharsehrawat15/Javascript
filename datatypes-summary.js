cb5bef18035e602175cbbb812c0cefa37673848e
tusharsehrawat15 <tusharsehrawat15@gmail.com>
2026-08-28 22:23:54 +0530

    Update datatypes-summary.js

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-consnole.log(id=== anotherId);
+console.log(id=== anotherId);
 
 // const bigNumber = 234343482959278237427n
 
