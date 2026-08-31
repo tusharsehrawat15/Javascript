efdcb78f081406110da164d550eeb778773ce086
tusharsehrawat15 <tusharsehrawat15@gmail.com>
2026-09-01 00:21:24 +0530

    Update 01_functions.js

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -39,3 +39,34 @@ function loginUserMessage(username){
 
 // console.log(loginUserMessage("Tushar"))
 console.log(loginUserMessage())
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    username: "tushar", 
+    price: 200
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// handleObject(user)
+
+handleObject({
+    username: "rahul",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 300, 500, 1000]))
