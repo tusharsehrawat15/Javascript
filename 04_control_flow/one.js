cc5330bf5c551a788621429030bf4f30c9695b16
tusharsehrawat15 <tusharsehrawat15@gmail.com>
2026-09-08 23:25:26 +0530

    Create one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+//if
+// const isUserloggedIn = true
+
+// if( 2 == "2"){
+//     console.log("executed");
+// }
+// else{
+//     console.log("not executed");
+// }
+
+// if(score>100){
+//     let power = "fly"
+//     console.log('User power: ${power}');
+// }
+
+// console.log('User power: ${power}');
+
+let balance = 1000;
+
+if(balance < 500){
+    console.log("less than");
+}
+else if(balance< 750){
+    console.log("less than 750");
+}
+else{
+    console.log("less than 1200")
+}
