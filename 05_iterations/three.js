22d875be112f5653202cca50e10344deb168c303
tusharsehrawat15 <tusharsehrawat15@gmail.com>
2026-09-09 00:08:59 +0530

    Create three.js

---
+++ 05_iterations/three.js
@@ -0,0 +1,29 @@
+// for of 
+
+// ["", "" , ""]
+// [{},{},{}]
+
+const arr = [1, 2, 3, 4 , 5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "Hello World"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+    
+}
+
+//Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
